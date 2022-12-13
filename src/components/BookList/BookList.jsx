e7970381f5863a3c062df64cbe090aa6b80cca02
Shisui <37116322+Shisui6@users.noreply.github.com>
2022-12-13 20:02:37 +0100

    Add empty message when booklist is empty

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -4,9 +4,25 @@ import { selectBooks } from '../../redux/books/books';
 import Book from '../Book/Book';
 import BookForm from '../BookForm/BookForm';
 import './BookList.css';
+import bookImg from '../../images/book-empty.png';
 
 const BookList = () => {
   const books = useSelector(selectBooks);
+
+  if (!books.length) {
+    return (
+      <>
+        <div className="booklist">
+          <div className="booklist-empty">
+            <img src={bookImg} alt="book" />
+            <h2>Library is empty. Add a book to see it here</h2>
+          </div>
+
+          <BookForm />
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
