45ba9d59560edcbe80e411e693a2d603073e7cec
Shisui <37116322+Shisui6@users.noreply.github.com>
2022-12-13 21:07:06 +0100

    Implement PR changes

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,12 +1,14 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './Categories.css';
-import { statusAction } from '../../redux/categories/categories';
+import { statusAction, selectCategories } from '../../redux/categories/categories';
 
 const Categories = () => {
   const dispatch = useDispatch();
+  const cat = useSelector(selectCategories);
 
   return (
     <div className="categories">
+      {cat.length ? <p>{cat}</p> : ''}
       <button type="button" onClick={() => dispatch(statusAction())}>Check status</button>
     </div>
   );
